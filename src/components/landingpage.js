e9a4d381836d439e1cf9f6c01799db88fcf51248
yangisha <yangishaa@gmail.com>
2020-07-22 11:39:38 +0545

    first commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+ 
+class Landing extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <React.Fragment>
+                <div style={(width:'100%',margin:'auto'}) >
+                <Grid className="landing-grid">
+                </Grid>
+
+                </div>
+
+            </React.Fragment>
+
+          );
+    }
+}
+ 
+export default Landing;
+ 
